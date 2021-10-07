9f8b5dbe989dc08023d557a9777720f719f43ddd
Katarinter <sr.g.jorge26@gmail.com>
2021-10-07 22:30:51 +0200

    Hito dos completado, get y post hechos

--- controller/mainController.js
+++ controller/mainController.js
@@ -1,10 +1,23 @@
-const prueba = {
-  status: 'The API works',
-  message: 'Main app route',
+const usuario = {
+  name: 'Jorge GO',
+  tlf: '666 777 999',
 };
+const User = require('../models/usuario');
 
-function getMain(req, res) {
-  return res.send(prueba);
+function getUsuario(req, res) {
+  return res.send(usuario);
+}
+function postUser(req, res) {
+  /* Coge el request.body con los atributos introducidos y lo devuelve con el metodo save() */
+
+  /* Global User, req.body */
+
+  const auxuser = new User(req.body);
+
+  auxuser.save((err, newUser) => {
+    if (err) return res.status(400).send({ message: 'Error al guardar el usuario', error: err });
+    return res.status(200).send(newUser);
+  });
 }
 
-module.exports = { getMain };
+module.exports = { getUsuario, postUser };
--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -3,6 +3,8 @@ const mainController = require('../controller/mainController');
 
 const router = Router();
 
-router.get('/', mainController.getMain);
+router.get('/', mainController.getUsuario);
+
+router.post('/', mainController.postUser);
 
 module.exports = router;
