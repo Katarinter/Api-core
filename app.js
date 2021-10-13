dfd829bdbd58f66a539a4c9b3a6cf3cfa34536dc
Katarinter <sr.g.jorge26@gmail.com>
2021-10-13 18:45:20 +0200

    Arreglado el metodo get

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
-const express = require('express');
+/*const express = require('express');
 // const mongoose = require('mongoose');
 const mainRoute = require('./routes/mainRoute');
 
 const app = express();
 const port = 3000;
 
+
 app.use(express.urlencoded({
   extended: false,
 }));
-app.use(express.json());
+app.use(express.json()); 
+
 app.use('/', mainRoute);
 
 app.listen(port, () => {
   console.log(`Node server running on http://localhost:${port}`);
 });
+*/
+
+const express = require('express'); // importa express
+
+// Rutas creadas
+const mainRoute = require('./routes/mainRoute');
+
+const app = express(); // Inicia la app
+
+app.use(express.urlencoded({ // conf para aceptar post request con distintas codificaciones
+  // true allows for rich objects to be encoded into the URL-encoded format allowing for a JSON-like
+  extended: false,
+}));
+app.use(express.json()); // only parses json
+
+// Rutas usadas
+app.use('/', mainRoute);
+
+module.exports = app;
