4f0a0c6597d6f5491fe690eb3c280a5aec3c5da6
Katarinter <sr.g.jorge26@gmail.com>
2021-10-17 22:28:11 +0200

    Arreglados los controllers3

--- controller/mainController.js
+++ controller/mainController.js
@@ -1,20 +1,19 @@
-const usuario = {
-  name: 'Jorge GO',
-  tlf: '666 777 999',
-};
 const User = require('../models/usuario');
 
 function getUsuario(req, res) {
-  return res.send(usuario);
+  User.find({}, (err, userData) => {
+    if (err) {
+      return res.status(400);
+    }
+    return res.send(userData);
+
+  });
 }
 function postUser(req, res) {
-  /* Coge el request.body con los atributos introducidos y lo devuelve con el metodo save() */
 
-  /* Global User, req.body */
+  const auxUser = new User(req.body);
 
-  const auxuser = new User(req.body);
-
-  auxuser.save((err, newUser) => {
+  auxUser.save((err, newUser) => {
     if (err) return res.status(400).send({ message: 'Error al guardar el usuario', error: err });
     return res.status(200).send(newUser);
   });
